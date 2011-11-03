3b23de134a0a499bdc39843468fb307923734919
David Griffiths <dogriffiths@gmail.com>
2011-11-03 10:50:09 +0000

    Main Python module

---
+++ rssgossip.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2011 by D+D Griffiths
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import urllib
+import os
+import sys
+from xml.dom import minidom
+
+url = os.environ['RSS_FEED']
+dom = minidom.parse(urllib.urlopen(url))
+forecasts = []
+for node in dom.getElementsByTagName('title'):
+    txt = node.firstChild.wholeText
+    if sys.argv[1] in txt:
+        print(txt)
+        os.system('say "%s" > /dev/null 2>&1' % txt.replace('"', '\"'))
