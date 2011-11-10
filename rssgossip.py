f0bb346bd54cc57d4e5a6b69b0dfd25c309802ae
David Griffiths <dogriffiths@gmail.com>
2011-11-10 11:18:00 +0000

    Added a -u option to display tab-indented URLs in addition to news titles.

--- rssgossip.py
+++ rssgossip.py
@@ -23,9 +23,30 @@ import re
 import sys
 import string
 import unicodedata
+import getopt
 from xml.dom import minidom
 
-searcher = re.compile(sys.argv[1], re.IGNORECASE)
+def usage():
+    print("Usage:\npython rssgossip.py [-uh] <search-regexp>")
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "uh", ["urls", "help"])
+except getopt.GetoptError, err:
+    print str(err)
+    usage()
+    sys.exit(2)
+
+include_urls = False
+for o, a in opts:
+    if o == "-u":
+        include_urls = True
+    elif o in ("-h", "--help"):
+        usage()
+        sys.exit()
+    else:
+        assert False, "unhandled option"
+
+searcher = re.compile(args[0], re.IGNORECASE)
 for url in string.split(os.environ['RSS_FEED']):
     feed = urllib.urlopen(url)
     try:
@@ -36,6 +57,9 @@ for url in string.split(os.environ['RSS_FEED']):
             if searcher.search(txt):
                 txt = unicodedata.normalize('NFKD', txt).encode('ascii', 'ignore')
                 print(txt)
-                #os.system('say "%s" > /dev/null 2>&1' % txt.replace('"', '\"'))
+                if include_urls:
+                    p = node.parentNode
+                    link = p.getElementsByTagName('link')[0].firstChild.wholeText
+                    print("\t%s" % link)
     except:
         sys.exit(1)
